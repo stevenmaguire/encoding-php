469ecf716ae66e9e8d8ecaaa1ac9f64f59c8c3e2
Steven Maguire <steven@stevenmaguire.com>
2015-02-02 19:55:50 -0600

    psr 2~

--- src/Stevenmaguire/EncodingDotCom/Model.php
+++ src/Stevenmaguire/EncodingDotCom/Model.php
@@ -157,12 +157,17 @@ abstract class Model implements Contracts\Jsonable
      *
      * @return string
      */
-    private function fromCamelCase($input) {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
-        $ret = $matches[0];
-        foreach ($ret as &$match) {
+    private function fromCamelCase($input)
+    {
+        preg_match_all(
+            '!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!',
+            $input,
+            $matches
+        );
+        $return = $matches[0];
+        foreach ($return as &$match) {
             $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
         }
-        return implode('_', $ret);
+        return implode('_', $return);
     }
 }
